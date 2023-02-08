9582309804d88c00180817219176971b482abee4
mohasin3639 <mohasinsayyad3639@gmail.com>
2023-02-08 19:44:08 +0530

    ADDED:A PROGRAM FROM CAB INVOICE

---
+++ Cab_Innvoice_genertor-master/Cab-Invoice/src/main/java/com/cabinvoicegenerator/InvoiceService.java
@@ -0,0 +1,73 @@
+package com.cabinvoicegenerator;
+
+import com.bridgelabz.cabinvoicegenerator.model.InvoiceSummary;
+import com.bridgelabz.cabinvoicegenerator.model.Ride;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InvoiceService {
+    private static final double COST_PER_KM = 10;
+    private static final double COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
+
+    public double calculateFare(double distance, int time) {
+//        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+//        if (totalFare < MIN_FARE) {
+//            return MIN_FARE;
+//        }
+//        return totalFare;
+
+
+        /*
+         ** Above can be refactored to
+         */
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
+    }
+
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare = calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+
+    }
+
+    public InvoiceSummary calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public InvoiceSummary getInvoice(int userId) {
+        Map<Integer, Ride[]> map = new HashMap<>();
+        Ride[] rides1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        Ride[] rides2 = {new Ride(5.0, 10),
+                new Ride(1, 1)
+        };
+
+        Ride[] rides3 = {new Ride(8.0, 15),
+                new Ride(1, 10)
+        };
+        map.put(1, rides1);
+        map.put(2, rides2);
+        map.put(3, rides3);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+            if (userId == entry.getKey()) {
+                System.out.println(entry.getKey());
+                Ride[] ridesArray = entry.getValue();
+                return calculateFare(ridesArray);
+            }
+        }
+        return null;
+    }
+}
---
+++ Cab_Innvoice_genertor-master/Cab-Invoice/src/test/java/com/cabinvoicegenerator/InvoiceServiceTest.java
@@ -0,0 +1,66 @@
+package com.cabinvoicegenerator;
+
+import com.bridgelabz.cabinvoicegenerator.model.InvoiceSummary;
+import com.bridgelabz.cabinvoicegenerator.model.Ride;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+    InvoiceService invoiceService;
+
+    @Before
+    public void setup() {
+        invoiceService = new InvoiceService();
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(25, totalFare, 0);
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 0.1;
+        int time = 1;
+        double totalFare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5.0, totalFare, 0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(5.0, 10),
+                new Ride(0.1, 1),
+                new Ride(20, 60)
+        };
+        InvoiceService invoiceService = new InvoiceService();
+        double totalFare = invoiceService.calculateTotalFare(rides);
+        Assert.assertEquals(260, totalFare, 0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        InvoiceService invoiceService = new InvoiceService();
+        InvoiceSummary invoiceSummary = invoiceService.calculateFare(rides);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
+
+    @Test
+    public void givenUserId_ShouldReturnInvoiceSummary() {
+
+        InvoiceSummary invoiceSummary = invoiceService.getInvoice(1);
+        InvoiceSummary expectedInvoices = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedInvoices.getInvoiceSummary(), invoiceSummary.getInvoiceSummary());
+    }
+}
